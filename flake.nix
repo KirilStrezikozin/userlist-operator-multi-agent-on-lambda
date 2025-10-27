efb84ec570de2df14373915c16418d2a05de51d0
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2025-10-27 19:33:49 +0100

    Add nix flake for local dev env setup
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Nix flake for local development";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    systems.url = "github:nix-systems/default-linux";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      systems,
+      ...
+    }:
+    let
+      inherit (nixpkgs) lib;
+      eachSystem = lib.genAttrs (import systems);
+
+      pkgsFor = eachSystem (
+        system:
+        import nixpkgs {
+          localSystem = system;
+          config = {
+            allowUnfree = true;
+          };
+        }
+      );
+
+    in
+    {
+      devShells = eachSystem (system: rec {
+        default = develop;
+
+        develop = pkgsFor.${system}.mkShell {
+          packages = with pkgsFor.${system}; [
+            uv
+            ruff
+            terraform
+            terraform-providers.aws
+            awscli2
+
+            pyright
+            nixd
+            terraform-lsp
+          ];
+
+          shellHook = ''		'';
+        };
+      });
+
+      formatter = eachSystem (system: pkgsFor.${system}.nixfmt);
+    };
+}
